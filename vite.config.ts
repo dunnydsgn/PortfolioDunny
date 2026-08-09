c87a380e725fe1451944a454f63f602b2a518d3b
dunnydsgn <dadasouza567@gmail.com>
2026-08-09 14:05:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+export default defineConfig({
+  base: '/PortfolioDunny/',
+  // ... restantes das configurações
+})
 
 function figmaAssetResolver() {
   return {
